6adabf9f633fc84357f1817e0276df480fc27c11
Jônatas <jonatas.dlp@gmail.com>
2025-08-17 18:31:32 -0300

    fix: avante integration

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -20,8 +20,8 @@ return {
 				model = "claude-sonnet-4-20250514",
 				timeout = 30000, -- Timeout in milliseconds
 				extra_request_body = {
-					temperature = 1,
-					max_tokens = 20000,
+					temperature = 0.75,
+					max_tokens = 20480,
 				},
 			},
 		},
