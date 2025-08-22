e848fbb3f241a8c8c762ad6eb86e607a8d8b0f66
Jonatas Santos <jonatas.dlp@gmail.com>
2025-08-22 20:44:18 -0300

    fix: line theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,7 @@ require("config.lazy")
 require("lualine").setup({
 	options = {
 		icons_enabled = true,
-		theme = "auto",
+		theme = "ayu_mirage",
 		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
 		disabled_filetypes = {
