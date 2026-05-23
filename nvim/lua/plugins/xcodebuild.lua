4e6e55a84f98741812c258458cf73379823282d4
Jonatas Santos <jonatas.dlp@gmail.com>
2026-05-23 19:50:36 -0300

    feat(xcodebuild): add fidget.nvim integration for build notifications and progress

--- nvim/lua/plugins/xcodebuild.lua
+++ nvim/lua/plugins/xcodebuild.lua
@@ -9,7 +9,41 @@ return {
 		"nvim-treesitter/nvim-treesitter", -- (optional) for Quick tests support (required Swift parser)
 	},
 	config = function()
-		require("xcodebuild").setup({})
+		local progress_handle
+
+		require("xcodebuild").setup({
+			show_build_progress_bar = false,
+			logs = {
+				notify = function(message, severity)
+					local fidget = require("fidget")
+					if progress_handle then
+						progress_handle.message = message
+						if not message:find("Loading") then
+							progress_handle:finish()
+							progress_handle = nil
+							if vim.trim(message) ~= "" then
+								fidget.notify(message, severity)
+							end
+						end
+					else
+						fidget.notify(message, severity)
+					end
+				end,
+				notify_progress = function(message)
+					local progress = require("fidget.progress")
+
+					if progress_handle then
+						progress_handle.title = ""
+						progress_handle.message = message
+					else
+						progress_handle = progress.handle.create({
+							message = message,
+							lsp_client = { name = "xcodebuild.nvim" },
+						})
+					end
+				end,
+			},
+		})
 
 		vim.keymap.set("n", "<leader>X", "<cmd>XcodebuildPicker<cr>", { desc = "Show Xcodebuild Actions" })
 		vim.keymap.set("n", "<leader>xf", "<cmd>XcodebuildProjectManager<cr>", { desc = "Show Project Manager Actions" })
